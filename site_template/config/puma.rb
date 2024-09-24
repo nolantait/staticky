8d13c6aae2461cc8c15abe7ec5477a83fa2b8627
Nolan J Tait <nolanjtait@gmail.com>
2024-09-23 20:26:34 -0700

    Adds puma as default webserver for site template

---
+++ site_template/config/puma.rb
@@ -0,0 +1,9 @@
+# Run in single threaded mode
+threads 0, 1
+
+# Specifies the `port` that Puma will listen on to receive requests; default is
+# 3000.
+port 3000
+
+# Only use a pidfile when requested
+pidfile ENV["PIDFILE"] if ENV["PIDFILE"]
