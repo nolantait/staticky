b5b521ca13510a8600846ee59102775a75190c4d
Nolan J Tait <nolanjtait@gmail.com>
2024-09-23 19:47:46 -0700

    Removes binstubs to be generated on install

--- lib/staticky/cli/commands/generate.rb
+++ lib/staticky/cli/commands/generate.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "debug"
+
 module Staticky
   module CLI
     module Commands
@@ -32,10 +34,34 @@ module Staticky
 
           Staticky.generator.call(path, **)
 
-          system(
-            "cd #{path} && chmod +x #{path}/bin/setup && #{path}/bin/setup",
-            chdir: path
-          ) || abort("install failed")
+          Dir.chdir(path) do
+            system("cd #{path}")
+            system("chmod +x bin/*")
+            bundle_command("install")
+            bundle_command("exec bin/setup")
+          end
+        end
+
+        def bundle_command(command, env = {})
+          puts "bundle #{command}"
+
+          # We are going to shell out rather than invoking Bundler::CLI.new(command)
+          # because `rails new` loads the Thor gem and on the other hand bundler uses
+          # its own vendored Thor, which could be a different version. Running both
+          # things in the same process is a recipe for a night with paracetamol.
+          #
+          # Thanks to James Tucker for the Gem tricks involved in this call.
+          _bundle_command = Gem.bin_path("bundler", "bundle")
+
+          require "bundler"
+          Bundler.with_original_env do
+            exec_bundle_command(_bundle_command, command, env)
+          end
+        end
+
+        def exec_bundle_command(bundle_command, command, env)
+          full_command = %Q["#{Gem.ruby}" "#{bundle_command}" #{command}]
+          system(env, full_command)
         end
       end
     end
