40c5d2faeded83239edd655d9d922db3c3562ad1
Nolan J Tait <nolanjtait@gmail.com>
2025-01-13 00:46:03 -0800

    Adds check for production on staticky env

--- lib/staticky/environment.rb
+++ lib/staticky/environment.rb
@@ -2,6 +2,7 @@
 
 module Staticky
   Environment = Data.define(:name) do
+    def production? = name == :production
     def development? = name == :development
     def test? = name == :test
   end
