bf46e2ba03927d98ac1aa2600304cf584bb43793
nolantait <nolanjtait@gmail.com>
2025-02-28 18:52:40 -0800

    Fixes error pages

--- site_template/app/views/errors/not_found.rb
+++ site_template/app/views/errors/not_found.rb
@@ -4,11 +4,12 @@ module Errors
   class NotFound < ApplicationPage
     include Protos::Typography
 
-    def self.layout = Layouts::Error
+    def around_template(&) = render Layouts::Error.new(&)
 
     def view_template
+      span(class: "font-thin text-8xl") { "404" }
       h1 { "Page not found." }
-      link_to("Back to home", Pages::Home, class: "link")
+      link_to("Back to home", Pages::Home, class: "btn mt-sm")
     end
   end
 end
--- site_template/app/views/errors/service_error.rb
+++ site_template/app/views/errors/service_error.rb
@@ -4,11 +4,12 @@ module Errors
   class ServiceError < ApplicationPage
     include Protos::Typography
 
-    def self.layout = Layouts::Error
+    def around_template(&) = render Layouts::Error.new(&)
 
     def view_template
+      span(class: "font-thin text-8xl") { "500" }
       h1 { "Something went wrong." }
-      link_to("Back to home", Pages::Home, class: "link")
+      link_to("Back to home", Pages::Home, class: "btn mt-sm")
     end
   end
 end
