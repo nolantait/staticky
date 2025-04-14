d76d8ae7a25096f2ed10e875190d663d167b7195
Nolan J Tait <nolanjtait@gmail.com>
2025-04-14 05:34:49 -0700

    Disables vite hmr on site template

--- site_template/vite.config.ts
+++ site_template/vite.config.ts
@@ -3,6 +3,8 @@ import RubyPlugin from "vite-plugin-ruby"
 import tailwindcss from "@tailwindcss/vite"
 
 export default defineConfig({
+  server: { hmr: false },
+  clearScreen: false,
   plugins: [
     tailwindcss(),
     RubyPlugin(),
